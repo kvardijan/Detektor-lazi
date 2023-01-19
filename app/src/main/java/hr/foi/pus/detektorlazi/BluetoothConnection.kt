1b3f1b728cdafe71f76f76341f234b30a5e54ebe
Karlo Vardijan <kvardijan20@foi.hr>
2023-01-19 21:32:44 +0100

    Implementirano povezivanje na ESP32 te privanje podataka u zasebnom threadu

---
+++ app/src/main/java/hr/foi/pus/detektorlazi/BluetoothConnection.kt
@@ -0,0 +1,83 @@
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.content.ContextCompat
+import java.io.IOException
+import java.util.*
+import androidx.core.app.ActivityCompat
+
+
+class BluetoothConnection {
+    private val REQUEST_BLUETOOTH = 1
+    private val TAG = "BluetoothConnection"
+    private var shouldStop = false
+
+    private var bluetoothAdapter: BluetoothAdapter? = null
+    private var device: BluetoothDevice? = null
+    private var socket: BluetoothSocket? = null
+
+    fun connect(address: String, activity: Activity) {
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN), REQUEST_BLUETOOTH)
+            return
+        }
+        //rest of the code
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        device = bluetoothAdapter?.getRemoteDevice(address)
+
+        try {
+            socket = device?.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
+            socket?.connect()
+            Log.d(TAG, "Connected to ESP32 device")
+        } catch (e: IOException) {
+            Log.e(TAG, "Failed to connect to ESP32 device", e)
+        }
+    }
+
+    fun receiveData(callback: (ByteArray?) -> Unit) {
+        Thread{
+            var i = 0
+            while (i < 30 && !shouldStop){
+                Log.d(TAG, "iteracija " + i)
+                try {
+                    val inputStream = socket?.inputStream
+                    val buffer = ByteArray(1024)
+                    Log.d(TAG, inputStream?.available().toString())
+                    val bytes = inputStream?.read(buffer)
+                    Log.d(TAG, "Received data: " + String(buffer))
+                    i++
+                    callback(buffer.copyOfRange(0, bytes!!))
+                    Thread.sleep(1000)
+                    //return buffer.copyOfRange(0, bytes!!)
+                } catch (e: IOException) {
+                    Log.e(TAG, "Failed to receive data from ESP32 device", e)
+                    callback(null)
+                    return@Thread
+                    //return null
+                } catch (e: InterruptedException) {
+                    Log.e(TAG, "Thread interrupted", e)
+                    callback(null)
+                    return@Thread
+                }
+            }
+        }.start()
+    }
+
+    fun stopThread(){
+        shouldStop = true
+    }
+
+    fun disconnect() {
+        try {
+            socket?.close()
+            Log.d(TAG, "Disconnected from ESP32 device")
+        } catch (e: IOException) {
+            Log.e(TAG, "Failed to disconnect from ESP32 device", e)
+        }
+    }
+}
---
+++ app/src/main/java/hr/foi/pus/detektorlazi/MainActivity.kt
@@ -0,0 +1,64 @@
+package hr.foi.pus.detektorlazi
+
+import BluetoothConnection
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+         val TAG = "BluetoothConnection"
+        var button = findViewById<Button>(R.id.btn_data)
+        var button2 = findViewById<Button>(R.id.btn_zaustavi)
+        var tekst = findViewById<TextView>(R.id.txt_data)
+        val bluetooth = BluetoothConnection()
+        button.setOnClickListener{
+            val REQUEST_BLUETOOTH = 1
+            Log.d(TAG, "kliknut gumb kreni")
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN), REQUEST_BLUETOOTH)
+            }
+
+            val bluetoothAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+            val pairedDevices: Set<BluetoothDevice> = bluetoothAdapter.bondedDevices
+
+            for (device in pairedDevices) {
+                Log.d(TAG, device.name + " " + device.address)
+                if (device.name == "Detektor laži") {
+                    Log.d(TAG, device.name + " " + device.address)
+
+
+                    bluetooth.connect(device.address, this)
+                    bluetooth.receiveData { data ->
+                        runOnUiThread {
+                            if (data != null)
+                                tekst.text = String(data)
+                            else
+                                tekst.text = "Error or thread interruption"
+                        }
+                    }
+                    //Log.d("Device: ", data.toString())
+                    //tekst.text = String(data!!)
+
+                }
+            }
+        }
+
+        button2.setOnClickListener {
+            bluetooth.disconnect()
+            Log.d(TAG, "kliknut gumb stani")
+            bluetooth.stopThread()
+        }
+    }
+}
